43a1c77e406a9d7dbee1b237e14eae0d9a2afda5
Jay Jackson <jaynickjackson@gmail.com>
2017-10-25 16:52:23 -0400

    added object donut intro

---
+++ 0.07_Objects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Donut PaulDonut = new Donut();
+
+            PaulDonut.Filling = "Cherry Jelly";
+            PaulDonut.Price = 7.99M;
+            PaulDonut.Type = "Jelly";
+            PaulDonut.IsSpecial = true;
+
+            Console.WriteLine(PaulDonut);
+            Console.WriteLine(PaulDonut.Filling);
+        }
+    }
+}
