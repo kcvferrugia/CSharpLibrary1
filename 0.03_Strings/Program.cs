331cd9f7e3a8ace8085c46aa8c592b30ebef575e
Jay Jackson <jaynickjackson@gmail.com>
2017-10-25 13:08:25 -0400

    added strings

---
+++ 0.03_Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._03_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string occupation;
+
+            firstName = "Fred";
+            lastName = "Rogers";
+            occupation = "Tv personality";
+
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            Console.WriteLine("Hello, my name is " + fullName + ". I am a " + occupation + ".");
+
+            //String Interpolation
+            Console.WriteLine("Hello, my name is {0}. I am a {1}.", fullName, occupation);
+
+            Console.WriteLine($"Hello, my name is {fullName}. I am a {occupation}.");
+
+        }
+    }
+}
---
+++ 0.03_StringsChallenges/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._03_StringsChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BRONZE
+            //Currency
+            double cash = 563.3265;
+            Console.WriteLine("{0:C}", cash);
+
+            //Percentage
+            double exchangeRate = 0.23;
+            Console.WriteLine("{0:#0.##%}", exchangeRate);
+            Console.WriteLine("{0:P}", exchangeRate);
+
+            //Phone Number
+            double phoneNumber = 8123648944;
+            string formatedPhoneNumber = phoneNumber.ToString("(###) ### - ####");
+            Console.WriteLine(formatedPhoneNumber);
+            Console.WriteLine("{0:(###) ### - ####}", phoneNumber);
+
+
+        }
+    }
+}
