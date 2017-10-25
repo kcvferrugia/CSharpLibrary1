0e4be97a1673a1735b31812f3403442bb496937d
Jay Jackson <jaynickjackson@gmail.com>
2017-10-25 13:52:26 -0400

    added conditionals2

--- 0.04_ConditionalsBankExercise/Program.cs
+++ 0.04_ConditionalsBankExercise/Program.cs
@@ -10,6 +10,25 @@ namespace _0._04_ConditionalsBankExercise
     {
         static void Main(string[] args)
         {
+            // Bank Exercise
+            var bankAccount = 10000;
+            var debt = 4200;
+            var difference = bankAccount - debt;
+
+            Console.WriteLine($"I have {bankAccount} in my bank account, and I am {debt} in debt.");
+
+            if (difference >= 700 && difference <= 1999)
+            {
+                Console.WriteLine("I have extra money. I should pay off my debt. I'll have $" + difference + " left.");
+            }
+            else if ((bankAccount - debt > 2000))
+            {
+                Console.WriteLine("I have a ton of extra cash right now. I think I could go to Europe!");
+            }
+            else
+            {
+                Console.WriteLine("It probably isn't a good time to pay off my debt.");
+            }
         }
     }
 }
