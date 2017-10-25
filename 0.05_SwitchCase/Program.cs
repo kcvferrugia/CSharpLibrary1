982ab023f20666b1e77a2d338636258e16e821b2
Jay Jackson <jaynickjackson@gmail.com>
2017-10-25 14:26:51 -0400

    added switch case

---
+++ 0.05_SwitchCase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._05_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Jay";
+            int age = 15;
+
+            switch (friend.ToLower())
+            {
+                case "jay":
+                    Console.WriteLine("Hey Jay.");
+                    break;
+                case "paul":
+                    Console.WriteLine("Paul is pretty cool.");
+                    break;
+                case "kenn":
+                    Console.WriteLine("Silent master Kenn...");
+                    break;
+                default:
+                    Console.WriteLine("I don't know you.");
+                    break;
+            }
+
+            switch (age)
+            {
+                case 21:
+                    Console.WriteLine("You can drink alcohol.");
+                    break;
+            }
+        }
+    }
+}
