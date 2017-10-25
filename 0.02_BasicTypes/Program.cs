8d7d87a60220cc7db2a565efa396ec8f21dfaa5f
Jay Jackson <jaynickjackson@gmail.com>
2017-10-25 11:14:50 -0400

    Initial commit

---
+++ 0.02_BasicTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte myByte = 255; // range: 0 - 255
+
+            int number1 = 15;
+
+            float myFloat = 3.14f;
+
+            double myDouble = 2.54;
+
+            decimal myDecimal = 3.14159M;
+
+            char myLetter = 'A';
+
+            string myName = "Jay";
+
+            bool myTruth = true;
+
+            // NOTES
+            // Declaration
+            // <type> <variableName> <optional initializer>;
+            int age;
+            string country;
+
+            // Initialization
+            age = 41;
+            Console.WriteLine(age);
+            age = 42;
+            Console.WriteLine(age);
+            Console.Beep();
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 0.02_BasicTypesChallenges/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypesChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BRONZE
+            int luckyNumber;
+            string firstName = "Jay";
+            string lastName = "Jackson";
+            bool isMarried = false;
+            double Pi = 3.14;
+
+            Console.WriteLine(Pi);
+            Console.WriteLine(isMarried);
+
+            //SILVER
+            string fullName = firstName + " " + lastName;
+            //Console.WriteLine(fullName);
+
+            //GOLD
+            string birthYear = "1980";
+            int year = Convert.ToInt32(birthYear);
+
+            string age = "30";
+            int numAge = Convert.ToInt32(age);
+            numAge = numAge + 1;
+
+            Console.WriteLine(numAge);
+
+
+
+            //Console.WriteLine("I was born in " + year);
+
+
+        }
+    }
+}
