599959aa4d0a22627deb738e2019c9dbed50ae99
Kathryn Ferrugia <kcvferrugia@gmail.com>
2017-11-02 09:40:56 -0400

    adding quiz

---
+++ 0.16_QuizOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._16_QuizOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number of triangle base");
+            decimal tbase = decimal.Parse(Console.ReadLine());
+
+            Console.Write("number of triangle height");
+            decimal theight = decimal.Parse(Console.ReadLine());
+
+            decimal area = (tbase * theight) / 2;
+
+            Console.WriteLine($"Area of triangle is: {0}");
+           
+        }
+            
+            public int Triangle { get; set; }
+
+            Triangle newTriangle = new triangle;
+
+            if Triangle < 6 
+                Console.WriteLine
+
+             Console.ReadLine();
+
+    }
+    }
+}
---
+++ 0.16_QuizOne/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._16_QuizOne
+{
+    class Triangle : Shape
+    {
+        //Constructor
+        public Triangle(int THeight, int TBase)
+        {
+            this.THeight = theight;
+            this.TBase = tbase
+        }
+
+        //Constants
+        public const int NUM_SIDES = 3;
+
+        //Properties
+        public int THeight { get; set; }
+        public int TBase { get; set; }
+
+        //Methods
+        public override double Area()
+        {
+            return ((TBase * THeight) / 2);
+        }
+    }
+}
